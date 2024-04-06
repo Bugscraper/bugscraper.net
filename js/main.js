cd61de02307304bb09f3f90ce70073f9f9740b7b
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2024-04-06 17:16:28 +0200

    Images

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let slideIndex = 1;
+updateSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+    slideIndex += n
+    updateSlides();
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    slideIndex = n
+    updateSlides();
+}
+
+function updateSlides() {
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
