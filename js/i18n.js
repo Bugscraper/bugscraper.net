053a936f3ca88931e24a9d6d03ee653f6ca9c7f9
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2024-06-07 14:27:42 +0200

    Testing new translations

--- js/i18n.js
+++ js/i18n.js
@@ -2,7 +2,9 @@
 
 const translations = {
     "en": {
-        "button-steam": "🐛 Wishlist on Steam",
+        "language-name": "🍔 English",
+
+        "button-steam": "🐛 Wishlist on Steam", // "wishlist" is a verb here, not a noun, as in "please wishlist on Steam"
         "button-itchio": "🏪 Play the demo on itch.io",
         "button-discord": "🎮 Discord", 
         "button-twitter": "🐦 Twitter", 
@@ -17,11 +19,16 @@ const translations = {
         "desc-long-3": "🐝 Using a wide variety of <b>weapons and upgrades</b>, eliminate them and prepare to battle for the next floor!",
         "desc-long-4": "🐞 Play in <b>solo</b>, or with up to 4 friends in <b>local co-op multiplayer</b>.",
         "screenshot-title": "Screenshots",
+
+        "translation-disclaimer": "Translations with [*] were made with the help of a generative AI model. Please get in touch if you'd like to help me with human-made translations!",
         "contact": "Contact:",
+        "email-disclaimer": "(might respond under a different address)",
         "footer-source": "Source code for this website",
         "footer-author": "Léo Bernard (Yolwoocle)"
     },
     "fr": {
+        "language-name": "🥖 Français",
+
         "button-steam": "🐛 Wishlistez sur Steam",
         "button-itchio": "🏪 Jouez à la démo sur itch.io",
         "button-discord": "🎮 Discord", 
@@ -31,15 +38,117 @@ const translations = {
         "button-email": "✉️ Email", 
         "desc-1": "Bienvenue dans le bugscraper.",
         "desc-2": "Ici se retrouvent des bêtes du monde entier.",
-        "desc-3": "Votre mission: les arrêter avant qu'il soit trop tard !",
+        "desc-3": "Votre mission : les arrêter avant qu'il ne soit trop tard !",
         "desc-long-1": "🐜 Dans ce jeu <b>de plateformes et de tir</b>, vous incarnez <b>Mio</b>, une courageuse fourmi, essayant d'empêcher les employés d'un gratte-ciel rempli de bestioles de corrompre le monde avec un champignon mortel.",
         "desc-long-2": "🐛 Vous affronterez dans un ascenseur des vagues d'ennemis qui viendront pour votre peau (ou plutôt, votre exosquelette) à chaque étage.",
         "desc-long-3": "🐝 À l'aide d'une grande variété <b>d'armes et d'améliorations</b>, éliminez-les et préparez-vous au combat pour l'étage suivant!",
         "desc-long-4": "🐞 Jouez en <b>solo</b>, ou avec jusqu'à 4 amis en <b>multijoueur local coopératif</b>.",
-    
         "screenshot-title": "Captures",
+    
+        "translation-disclaimer": "Les traductions avec (*) ont été réalisées à l'aide d'un modèle d'IA générative. Veuillez me contacter si vous souhaitez m'aider à fournir des traductions faites par des humains !",
         "contact": "Contact :",
+        "email-disclaimer": "(les réponses peuvent provenir d'une adresse différente)",
         "footer-source": "Code source de ce site web",
+        "footer-author": "Léo Bernard (Yolwoocle)"
+    },
+    "es": {
+        "language-name": "🌮 Español (*)",
+
+        "button-steam": "🐛 Añádelo a tu lista de deseos en Steam",
+        "button-itchio": "🏪 Juega la demo en itch.io",
+        "button-discord": "🎮 Discord", 
+        "button-twitter": "🐦 Twitter", 
+        "button-github": "😺 GitHub", 
+        "button-press": "📰 Kit de prensa",
+        "button-email": "✉️ Correo electrónico",
+        "desc-1": "Bienvenido al bugscraper.",
+        "desc-2": "Aquí se reúnen plagas de todo el mundo.",
+        "desc-3": "Tu misión: detenerlas antes de que sea demasiado tarde!",
+        "desc-long-1": "🐜 En este juego de plataformas y disparos, tomas el papel de <b>Mio</b>, una valiente hormiga, que intenta evitar que los empleados de un rascacielos lleno de bichos corrompan el mundo con un hongo mortal.",
+        "desc-long-2": "🐛 Lucharás contra oleadas de enemigos en un ascensor mientras vienen por tu piel (o más bien, tu exoesqueleto) en cada piso.",
+        "desc-long-3": "🐝 Usando una gran variedad de <b>armas y mejoras</b>, elimínalos y prepárate para luchar en el siguiente piso!",
+        "desc-long-4": "🐞 Juega en <b>solitario</b> o con hasta 4 amigos en <b>cooperativo local</b>.",
+        "screenshot-title": "Capturas de pantalla",
+
+        "translation-disclaimer": "Las traducciones con (*) se hicieron con la ayuda de un modelo de IA generativa. ¡Por favor, póngase en contacto si desea ayudarme con traducciones hechas por humanos!",
+        "contact": "Contacto:",
+        "email-disclaimer": "(puede responder desde una dirección diferente)",
+        "footer-source": "Código fuente de este sitio web",
+        "footer-author": "Léo Bernard (Yolwoocle)"
+    },
+    "de": {
+        "language-name": "🍺 Deutsch (*)",
+
+        "button-steam": "🐛 Zur Wunschliste auf Steam hinzufügen",
+        "button-itchio": "🏪 Demo auf itch.io spielen",
+        "button-discord": "🎮 Discord", 
+        "button-twitter": "🐦 Twitter", 
+        "button-github": "😺 GitHub", 
+        "button-press": "📰 Pressekit",
+        "button-email": "✉️ E-Mail",
+        "desc-1": "Willkommen beim Bugscraper.",
+        "desc-2": "Hier versammeln sich Schädlinge aus aller Welt.",
+        "desc-3": "Deine Mission: Sie aufhalten, bevor es zu spät ist!",
+        "desc-long-1": "🐜 In diesem Plattform-Shooter übernimmst du die Rolle von <b>Mio</b>, einer mutigen Ameise, die versucht, die Mitarbeiter eines mit Käfern gefüllten Wolkenkratzers daran zu hindern, die Welt mit einem tödlichen Pilz zu verderben.",
+        "desc-long-2": "🐛 Du wirst in einem Aufzug gegen Wellen von Feinden kämpfen, die auf jeder Etage nach deiner Haut (bzw. deinem Exoskelett) trachten.",
+        "desc-long-3": "🐝 Mit einer Vielzahl von <b>Waffen und Upgrades</b> eliminierst du sie und bereitest dich auf den Kampf in der nächsten Etage vor!",
+        "desc-long-4": "🐞 Spiele im <b>Einzelspielermodus</b> oder mit bis zu 4 Freunden im <b>lokalen Koop-Modus</b>.",
+        "screenshot-title": "Screenshots",
+
+        "translation-disclaimer": "Übersetzungen mit (*) wurden mit Hilfe eines generativen KI-Modells erstellt. Bitte kontaktieren Sie mich, wenn Sie mir mit menschlichen Übersetzungen helfen möchten!",
+        "contact": "Kontakt:",
+        "email-disclaimer": "(Antworten können von einer anderen Adresse kommen)",
+        "footer-source": "Quellcode dieser Webseite",
+        "footer-author": "Léo Bernard (Yolwoocle)"
+    },
+    "it": {
+        "language-name": "🍝 Italiano (*)",
+
+        "button-steam": "🐛 Aggiungi alla lista dei desideri su Steam",
+        "button-itchio": "🏪 Gioca la demo su itch.io",
+        "button-discord": "🎮 Discord", 
+        "button-twitter": "🐦 Twitter", 
+        "button-github": "😺 GitHub", 
+        "button-press": "📰 Cartella stampa",
+        "button-email": "✉️ Email",
+        "desc-1": "Benvenuto al bugscraper.",
+        "desc-2": "Qui si radunano parassiti da tutto il mondo.",
+        "desc-3": "La tua missione: fermarli prima che sia troppo tardi!",
+        "desc-long-1": "🐜 In questo platform shooter, interpreti il ruolo di <b>Mio</b>, una coraggiosa formica che cerca di impedire ai dipendenti di un grattacielo pieno di insetti di corrompere il mondo con un fungo mortale.",
+        "desc-long-2": "🐛 Combatterai ondate di nemici in un ascensore mentre cercano la tua pelle (o meglio, il tuo esoscheletro) ad ogni piano.",
+        "desc-long-3": "🐝 Usando una vasta gamma di <b>armi e potenziamenti</b>, eliminali e preparati a combattere per il prossimo piano!",
+        "desc-long-4": "🐞 Gioca in <b>solitario</b> o con un massimo di 4 amici in <b>coop locale</b>.",
+        "screenshot-title": "Screenshot",
+
+        "translation-disclaimer": "Le traduzioni con (*) sono state realizzate con l'aiuto di un modello di IA generativa. Per favore, contattami se desideri aiutarmi con traduzioni fatte da umani!",
+        "contact": "Contatto:",
+        "email-disclaimer": "(potrebbe rispondere da un indirizzo diverso)",
+        "footer-source": "Codice sorgente di questo sito web",
+        "footer-author": "Léo Bernard (Yolwoocle)"
+    },
+    "pt": {
+        "language-name": "🍛 Português (*)",
+
+        "button-steam": "🐛 Adicione à sua lista de desejos no Steam",
+        "button-itchio": "🏪 Jogue a demo no itch.io",
+        "button-discord": "🎮 Discord", 
+        "button-twitter": "🐦 Twitter", 
+        "button-github": "😺 GitHub", 
+        "button-press": "📰 Kit de imprensa",
+        "button-email": "✉️ Email",
+        "desc-1": "Bem-vindo ao bugscraper.",
+        "desc-2": "Aqui, pragas de todo o mundo se reúnem.",
+        "desc-3": "Sua missão: detê-las antes que seja tarde demais!",
+        "desc-long-1": "🐜 Neste jogo de plataforma e tiro, você assume o papel de <b>Mio</b>, uma corajosa formiga que tenta impedir que os funcionários de um arranha-céu cheio de insetos corrompam o mundo com um fungo mortal.",
+        "desc-long-2": "🐛 Você lutará contra ondas de inimigos em um elevador enquanto eles vêm atrás da sua pele (ou melhor, do seu exoesqueleto) em cada andar.",
+        "desc-long-3": "🐝 Usando uma grande variedade de <b>armas e melhorias</b>, elimine-os e prepare-se para lutar no próximo andar!",
+        "desc-long-4": "🐞 Jogue em <b>solo</b> ou com até 4 amigos em <b>cooperativo local</b>.",
+        "screenshot-title": "Capturas de tela",
+
+        "translation-disclaimer": "As traduções com (*) foram feitas com a ajuda de um modelo de IA generativa. Por favor, entre em contato se quiser me ajudar com traduções feitas por humanos!",
+        "contact": "Contato:",
+        "email-disclaimer": "(as respostas podem vir de um endereço diferente)",
+        "footer-source": "Código-fonte deste site",
         "footer-author": "Léo Bernard (Yolwoocle)"
     },
 };
@@ -100,7 +209,7 @@ function saveLocale() {
 
 function translateElement(element) {
     const key = element.getAttribute("data-i18n-key");
-    const translation = translations[locale][key];
+    const translation = translations[locale][key] ?? translations[defaultLocale][key];
     element.innerHTML = translation;
 }
 
